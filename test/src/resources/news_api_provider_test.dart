9ec24998f4a14049dd1a4f075d1527553e16ec06
SaraW011 <sarawsmn@gmail.com>
2023-12-21 21:29:44 +0200

    creating test file basics

---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+import 'package:test/test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main() {
+  test('fetchTopIds returns a list of ids', () {
+    // setup of test case
+  });
+}
