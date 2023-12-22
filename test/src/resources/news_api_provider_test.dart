a70381df743fe41a6a1364aa63d603b047fa21e1
SaraW011 <sarawsmn@gmail.com>
2023-12-22 14:38:55 +0200

    setting up test - no pass

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_news_api/src/resources/news_api_provider.dart';
 import 'dart:convert';
 import 'package:test/test.dart';
 import 'package:http/http.dart';
@@ -5,6 +6,14 @@ import 'package:http/testing.dart';
 
 void main() {
   test('fetchTopIds returns a list of ids', () {
-    // setup of test case
+    //setup of test case
+    //better if test doesn't reach out to external api,
+    //so use http package "MockClient" to complete test without reachig external
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient(
+      (request) async => Response(json.encode([1, 2, 3, 4]), 200),
+    );
+    final ids = newsApi.fetchTopIds();
+    expect(ids, [1, 2, 3, 4]);
   });
 }
