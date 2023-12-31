7ee5f8278d9a000a1f1b4c8b4e4b806c4fb801d7
SaraW011 <sarawsmn@gmail.com>
2023-12-31 07:11:55 +0200

    repo fetch item from api/db

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -5,11 +5,25 @@ import 'news_api_provider.dart';
 import 'news_db_provider.dart';
 import '../models/item_model.dart';
 
+//governs access to either of the providers:
 class Repository {
   //db must be first initialized and then get data
   NewsDbProvider dbProvider = NewsDbProvider();
   NewsApiProvider apiProvider = NewsApiProvider();
 
-  fetchTopIds() {}
-  fetchItem() {}
+//does not store top ids, only calls api
+  fetchTopIds() {
+    return apiProvider.fetchTopIds();
+  }
+
+  fetchItem(int id) async {
+    var item = await dbProvider.fetchItem(id);
+    if (item != null) {
+      return item;
+    }
+    //reasign var "item" to new variable:
+    item = await apiProvider.fetchItem(id);
+    dbProvider.addItem(item);
+    return item;
+  }
 }
