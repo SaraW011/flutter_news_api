d4a6cd7b5b1416b848a85c5713b440c53bcecfba
SaraW011 <sarawsmn@gmail.com>
2024-01-09 13:30:33 +0200

    temp fix null issues

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'package:flutter_news_api/src/resources/news_db_provider.dart';
-
 import 'news_api_provider.dart';
 import 'news_db_provider.dart';
 import '../models/item_model.dart';
@@ -24,7 +22,7 @@ class Repository {
   // NewsApiProvider apiProvider = NewsApiProvider();
 
 //does not store top ids, only calls api
-  Future<List<int>> fetchTopIds() {
+  Future<List<int>>? fetchTopIds() {
     // return apiProvider.fetchTopIds();
 
     //ToDo: iterate over sources when fetchTopIds implemented
@@ -38,14 +36,16 @@ class Repository {
 
     for (source in sources) {
       item = await source.fetchItem(id);
-      if (item != null) {
-        break;
+      // if (item != null) {
+      //   break;
+      // }
+      for (var cache in caches) {
+        cache.addItem(item);
       }
+      return item;
     }
-    for (var cache in caches) {
-      cache.addItem(item);
-    }
-    return item;
+    //cannot return null so:
+    throw 'Out of llamas!';
   }
 
   //not refractored:
