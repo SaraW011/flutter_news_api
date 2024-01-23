9d10a396feaa7210b108c3f88c9a66042c620745
SaraW011 <sarawsmn@gmail.com>
2024-01-23 14:32:20 +0200

    FutureBuilder

--- lib/src/widgets/news_list_tile.dart
+++ lib/src/widgets/news_list_tile.dart
@@ -5,22 +5,30 @@ import '/src/blocs/stories_provider.dart';
 import 'dart:async';
 
 class NewsListTile extends StatelessWidget {
-  const NewsListTile({super.key, required this.itmeId});
+  const NewsListTile({super.key, required this.itemId});
 
-  final int itmeId;
+  final int itemId;
 
   @override
   Widget build(BuildContext context) {
     final bloc = StoriesProvider.of(context);
 
     return StreamBuilder(
-      stream: bloc.items,
-      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
-        if (!snapshot.hasData) {
-          return const Text('Stream Loading...');
-        }
-        throw 'Out of llamas!';
-      },
-    );
+        stream: bloc.items,
+        builder:
+            (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+          if (!snapshot.hasData) {
+            return const Text('Stream Loading...');
+          }
+          // throw 'Out of llamas!';
+          return FutureBuilder(
+              future: snapshot.data?[itemId],
+              builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
+                if (!itemSnapshot.hasData) {
+                  return Text('Loading item $itemId...');
+                }
+                return Text(itemSnapshot.data!.title);
+              });
+        });
   }
 }
